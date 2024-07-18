af31bf2d1e57db4754a9065e10fd1babbc1730e3
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-07-18 23:19:14 +0530

    fix: ✨🍉 trying to connect quickly

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -6,16 +6,9 @@ export const pusherServer = new PusherServer({
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: 'ap2',
-  useTLS: true,
 })
 
 export const pusherClient = new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
   cluster: 'ap2',
   authEndpoint: '/api/pusher-auth',
-  authTransport: 'ajax',
-  auth: {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  },
 })
